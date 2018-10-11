ced45f9337698331fd5b89d03859424e3db3a6d3
Kiss Norbert <nn@norbilaptop.lan>
2018-10-11 17:43:39 +0200

    develope

--- src/ProgramozasiTetelek/Rendezes/BuborekRendezes.java
+++ src/ProgramozasiTetelek/Rendezes/BuborekRendezes.java
@@ -1,23 +1,27 @@
 /**
  * Feltételezzük, hogy az n a tömb elemeinek száma.
- * A sorozat két első elemét összehasonlítjuk, és ha fordított sorrendben vannak felcseréljük. Utána a másodikat és a harmadikat hasonlítom össze.
- * Ha a nagyobb számokat a végén szeretném látni, akkor az első körben a legnagyobb szám tulajdonképpen a sor végére kerül. Ezért a következő körben
- * azzal már nem foglalkozunk, ezért megyünk mindig a belső ciklusban csak i változó értékéig. A külső ciklus így megmutatja meddig kell mennünk.
+ * A sorozat két első elemét összehasonlítjuk, és ha fordított sorrendben vannak felcseréljük. Utána
+ * a másodikat és a harmadikat hasonlítom össze.
+ * Ha a nagyobb számokat a végén szeretném látni, akkor az első körben a legnagyobb szám
+ * tulajdonképpen a sor végére kerül. Ezért a következő körben
+ * azzal már nem foglalkozunk, ezért megyünk mindig a belső ciklusban csak i változó értékéig. A
+ * külső ciklus így megmutatja meddig kell mennünk.
  */
 package ProgramozasiTetelek.Rendezes;
 
 public class BuborekRendezes {
 
     public static void main(String[] args) {
-        int[] tomb = {9, 4, 5, 2, 3, 1, 6, 8, 7, 10};
+        int[] tomb = { 9, 4, 5, 2, 3, 1, 6, 8, 7, 10 };
 
-        for (int i = 0; i < tomb.length - 1; i++) {
-            if (tomb[i] > tomb[i + 1]) {
-                int a = tomb[i];
-                int b = tomb[i + 1];
-                tomb[i] = b;
-                tomb[i + 1] = a;
-                i = 0;
+        for (int i = tomb.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (tomb[j] > tomb[i]) {
+                    int a = tomb[i];
+                    tomb[i] = tomb[j];
+                    tomb[j] = a;
+                }
+
             }
         }
 
